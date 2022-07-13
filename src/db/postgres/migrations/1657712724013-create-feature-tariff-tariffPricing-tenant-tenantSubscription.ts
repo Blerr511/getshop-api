fd372956567f96118678b619df1bfce45bb1a017
Vahe Hovhannisyan <vahe.hovhannisyan@aod.am>
2022-07-13 15:46:07 +0400

    Entities, migrations implementation

--- src/db/postgres/migrations/1657712724013-create-feature-tariff-tariffPricing-tenant-tenantSubscription.ts
+++ src/db/postgres/migrations/1657712724013-create-feature-tariff-tariffPricing-tenant-tenantSubscription.ts
@@ -1,17 +1,14 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class createFeatureTariffTariffPricingTenantTenantSubscription1657712138083
+export class createFeatureTariffTariffPricingTenantTenantSubscription1657712724013
   implements MigrationInterface
 {
   name =
-    'createFeatureTariffTariffPricingTenantTenantSubscription1657712138083';
+    'createFeatureTariffTariffPricingTenantTenantSubscription1657712724013';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "feature" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "description" character varying NOT NULL, CONSTRAINT "uniqueFeatureName" UNIQUE ("name"), CONSTRAINT "PK_03930932f909ca4be8e33d16a2d" PRIMARY KEY ("id"))`,
-    );
-    await queryRunner.query(
-      `CREATE TABLE "tenant" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "uniqueTenantName" UNIQUE ("name"), CONSTRAINT "PK_da8c6efd67bb301e810e56ac139" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "tenant" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_TENANT_NAME" UNIQUE ("name"), CONSTRAINT "PK_da8c6efd67bb301e810e56ac139" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `CREATE TABLE "tenant_subscription" ("id" SERIAL NOT NULL, "active_from" TIMESTAMP NOT NULL, "active_until" TIMESTAMP NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "tenant_id" integer, "tariff_id" integer, CONSTRAINT "REL_a300c9bff7b5bb7cd6d3ed00e7" UNIQUE ("tenant_id"), CONSTRAINT "PK_d91b66c187ed664d890944a9776" PRIMARY KEY ("id"))`,
@@ -20,7 +17,10 @@ export class createFeatureTariffTariffPricingTenantTenantSubscription16577121380
       `CREATE TABLE "tariff_pricing" ("id" SERIAL NOT NULL, "days" integer NOT NULL, "price" integer NOT NULL, "sale_price" integer NOT NULL, "tariff_id" integer, CONSTRAINT "PK_3af69322deb5a8a8345bbe48de5" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
-      `CREATE TABLE "tariff" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "description" character varying NOT NULL, CONSTRAINT "uniqueTariffName" UNIQUE ("name"), CONSTRAINT "PK_bbeac9df199ea1c22c6dea75c2f" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "tariff" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "description" character varying NOT NULL, CONSTRAINT "UQ_TARIFF_NAME" UNIQUE ("name"), CONSTRAINT "PK_bbeac9df199ea1c22c6dea75c2f" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "feature" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "description" character varying NOT NULL, CONSTRAINT "UQ_FEATURE_NAME" UNIQUE ("name"), CONSTRAINT "PK_03930932f909ca4be8e33d16a2d" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `CREATE TABLE "feature_tariff_tariff" ("feature_id" integer NOT NULL, "tariff_id" integer NOT NULL, CONSTRAINT "PK_c78ec2d53591f106b3b4a5793df" PRIMARY KEY ("feature_id", "tariff_id"))`,
@@ -71,10 +71,10 @@ export class createFeatureTariffTariffPricingTenantTenantSubscription16577121380
       `DROP INDEX "public"."IDX_3d19302780ce779734a86370f6"`,
     );
     await queryRunner.query(`DROP TABLE "feature_tariff_tariff"`);
+    await queryRunner.query(`DROP TABLE "feature"`);
     await queryRunner.query(`DROP TABLE "tariff"`);
     await queryRunner.query(`DROP TABLE "tariff_pricing"`);
     await queryRunner.query(`DROP TABLE "tenant_subscription"`);
     await queryRunner.query(`DROP TABLE "tenant"`);
-    await queryRunner.query(`DROP TABLE "feature"`);
   }
 }
